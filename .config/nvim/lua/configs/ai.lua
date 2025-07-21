a4b0784d0a3364eeceae07142f6f1e4f95969a7c
Barun Debnath <barundebnath91@gmail.com>
2025-07-21 11:16:10 +0530

    dotfiles: nvim configuration updated

--- .config/nvim/lua/configs/ai.lua
+++ .config/nvim/lua/configs/ai.lua
@@ -11,96 +11,96 @@ return {
   --     },
   --   }),
   -- }
-  require("codecompanion").setup({
-    opts = {
-      log_level = "info",
-    },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          make_vars = true,
-          make_slash_commands = true,
-          show_result_in_chat = true
-        }
-      },
-      history = {
-        enabled = true,
-        opts = {
-          -- Keymap to open history from chat buffer (default: gh)
-          keymap = "<leader>gh",
-          -- Keymap to save the current chat manually (when auto_save is disabled)
-          save_chat_keymap = "<leader>gc",
-          -- Save all chats by default (disable to save only manually using 'sc')
-          auto_save = true,
-          -- Number of days after which chats are automatically deleted (0 to disable)
-          expiration_days = 0,
-          -- Picker interface (auto resolved to a valid picker)
-          picker = "snacks", --- ("telescope", "snacks", "fzf-lua", or "default")
-          ---Automatically generate titles for new chats
-          auto_generate_title = true,
-          title_generation_opts = {
-            ---Adapter for generating titles (defaults to current chat adapter)
-            adapter = "copilot", -- "copilot"
-            ---Model for generating titles (defaults to current chat model)
-            model = "gpt-4o",    -- "gpt-4o"
-          },
-          ---On exiting and entering neovim, loads the last chat on opening chat
-          continue_last_chat = true,
-          ---When chat is cleared with `gx` delete the chat from history
-          delete_on_clearing_chat = false,
-          ---Directory path to save the chats
-          dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
-          ---Enable detailed logging for history extension
-          enable_logging = false,
-        }
-      },
-      vectorcode = {
-        opts = {
-          add_tool = true,
-        }
-      }
-    },
-    strategies = {
-      chat = {
-        adapter = "copilot"
-      },
-      inline = {
-        adapter = "copilot",
-        keymaps = {
-          accept_change = {
-            modes = { n = "ga" },
-            description = "Accept the suggested change",
-          },
-          send = {
-            callback = function(chat)
-              vim.cmd("stopinsert")
-              chat:submit()
-            end,
-            index = 1,
-            description = "Send",
-          },
-          reject_change = {
-            modes = { n = "gr" },
-            description = "Reject the suggested change",
-          },
-        },
-      },
-      cmd = {
-        adapter = "copilot",
-      }
-    },
-    display = {
-      action_palette = {
-        width = 95,
-        height = 10,
-        prompt = "Prompt ",                   -- Prompt used for interactive LLM calls
-        provider = "snacks",                  -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
-        opts = {
-          show_default_actions = true,        -- Show the default actions in the action palette?
-          show_default_prompt_library = true, -- Show the default prompt library in the action palette?
-        },
-      }
-    }
-  })
+  -- require("codecompanion").setup({
+  --   opts = {
+  --     log_level = "info",
+  --   },
+  --   extensions = {
+  --     mcphub = {
+  --       callback = "mcphub.extensions.codecompanion",
+  --       opts = {
+  --         make_vars = true,
+  --         make_slash_commands = true,
+  --         show_result_in_chat = true
+  --       }
+  --     },
+  --     history = {
+  --       enabled = true,
+  --       opts = {
+  --         -- Keymap to open history from chat buffer (default: gh)
+  --         keymap = "<leader>gh",
+  --         -- Keymap to save the current chat manually (when auto_save is disabled)
+  --         save_chat_keymap = "<leader>gc",
+  --         -- Save all chats by default (disable to save only manually using 'sc')
+  --         auto_save = true,
+  --         -- Number of days after which chats are automatically deleted (0 to disable)
+  --         expiration_days = 0,
+  --         -- Picker interface (auto resolved to a valid picker)
+  --         picker = "snacks", --- ("telescope", "snacks", "fzf-lua", or "default")
+  --         ---Automatically generate titles for new chats
+  --         auto_generate_title = true,
+  --         title_generation_opts = {
+  --           ---Adapter for generating titles (defaults to current chat adapter)
+  --           adapter = "copilot", -- "copilot"
+  --           ---Model for generating titles (defaults to current chat model)
+  --           model = "gpt-4o",    -- "gpt-4o"
+  --         },
+  --         ---On exiting and entering neovim, loads the last chat on opening chat
+  --         continue_last_chat = true,
+  --         ---When chat is cleared with `gx` delete the chat from history
+  --         delete_on_clearing_chat = false,
+  --         ---Directory path to save the chats
+  --         dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+  --         ---Enable detailed logging for history extension
+  --         enable_logging = false,
+  --       }
+  --     },
+  --     vectorcode = {
+  --       opts = {
+  --         add_tool = true,
+  --       }
+  --     }
+  --   },
+  --   strategies = {
+  --     chat = {
+  --       adapter = "copilot"
+  --     },
+  --     inline = {
+  --       adapter = "copilot",
+  --       keymaps = {
+  --         accept_change = {
+  --           modes = { n = "ga" },
+  --           description = "Accept the suggested change",
+  --         },
+  --         send = {
+  --           callback = function(chat)
+  --             vim.cmd("stopinsert")
+  --             chat:submit()
+  --           end,
+  --           index = 1,
+  --           description = "Send",
+  --         },
+  --         reject_change = {
+  --           modes = { n = "gr" },
+  --           description = "Reject the suggested change",
+  --         },
+  --       },
+  --     },
+  --     cmd = {
+  --       adapter = "copilot",
+  --     }
+  --   },
+  --   display = {
+  --     action_palette = {
+  --       width = 95,
+  --       height = 10,
+  --       prompt = "Prompt ",                   -- Prompt used for interactive LLM calls
+  --       provider = "snacks",                  -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+  --       opts = {
+  --         show_default_actions = true,        -- Show the default actions in the action palette?
+  --         show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+  --       },
+  --     }
+  --   }
+  -- })
 }
--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,40 +1,40 @@
 return {
-  { "github/copilot.vim" },
-  {
-    "olimorris/codecompanion.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "ravitemer/mcphub.nvim",
-      "j-hui/fidget.nvim",
-      "ravitemer/codecompanion-history.nvim"
-    },
-    -- init = function()
-    --   require("plugins.fidget"):init()
-    -- end,
-    config = function(_, opts)
-      local spinner = require("configs.spinner")
-      spinner:init()
-      -- Setup the entire opts table
-      require("codecompanion").setup(opts)
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    config = function() require("copilot_cmp").setup() end,
-    dependencies = {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        })
-      end,
-    },
-  },
+  -- { "github/copilot.vim" },
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   opts = {},
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "ravitemer/mcphub.nvim",
+  --     "j-hui/fidget.nvim",
+  --     "ravitemer/codecompanion-history.nvim"
+  --   },
+  --   -- init = function()
+  --   --   require("plugins.fidget"):init()
+  --   -- end,
+  --   config = function(_, opts)
+  --     local spinner = require("configs.spinner")
+  --     spinner:init()
+  --     -- Setup the entire opts table
+  --     require("codecompanion").setup(opts)
+  --   end,
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   config = function() require("copilot_cmp").setup() end,
+  --   dependencies = {
+  --     "zbirenbaum/copilot.lua",
+  --     cmd = "Copilot",
+  --     config = function()
+  --       require("copilot").setup({
+  --         suggestion = { enabled = false },
+  --         panel = { enabled = false },
+  --       })
+  --     end,
+  --   },
+  -- },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown", "codecompanion" }
@@ -71,34 +71,34 @@ return {
       },
     },
   },
-  {
-    "ravitemer/mcphub.nvim",
-    build = "npm install -g mcp-hub@latest",
-    config = function()
-      require("mcphub").setup()
-    end
-  },
-  {
-    "Davidyz/VectorCode",
-    version = "*",                     -- optional, depending on whether you're on nightly or release
-    build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = function()
-      return {
-        async_backend = "lsp",
-        notify = true,
-        on_setup = { lsp = false },
-        n_query = 10,
-        timeout_ms = -1,
-        async_opts = {
-          events = { "BufWritePost" },
-          single_job = true,
-          --query_cb = require("vectorcode.utils").make_surrounding_lines_cb(40),
-          debounce = -1,
-          n_query = 30,
-        },
-      }
-    end,
+  -- {
+  --   "ravitemer/mcphub.nvim",
+  --   build = "npm install -g mcp-hub@latest",
+  --   config = function()
+  --     require("mcphub").setup()
+  --   end
+  -- },
+  -- {
+    -- "Davidyz/VectorCode",
+    -- version = "*",                     -- optional, depending on whether you're on nightly or release
+    -- build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
+    -- dependencies = { "nvim-lua/plenary.nvim" },
+    -- opts = function()
+    --   return {
+    --     async_backend = "lsp",
+    --     notify = true,
+    --     on_setup = { lsp = false },
+    --     n_query = 10,
+    --     timeout_ms = -1,
+    --     async_opts = {
+    --       events = { "BufWritePost" },
+    --       single_job = true,
+    --       --query_cb = require("vectorcode.utils").make_surrounding_lines_cb(40),
+    --       debounce = -1,
+    --       n_query = 30,
+    --     },
+    --   }
+    -- end,
     -- config = function(_, opts)
     --   vim.lsp.config("vectorcode_server", {
     --     cmd_env = {
@@ -113,12 +113,12 @@ return {
     --   --   end,
     --   -- })
     -- end,
-    cmd = "VectorCode",
+    -- cmd = "VectorCode",
     -- cond = function()
     --   return vim.fn.executable("vectorcode") == 1 and utils.no_vscode()
     -- end,
 
-  }
+--  }
 
   -- {
   --   "Davidyz/VectorCode",
